fa02ee4a930b5bb2baebda9678d6dbd3e238d386
M. Daniela Robledo Akerman <danielaakerman14@gmail.com>
2022-09-27 00:00:43 -0300

    commit

--- proyecto-II.hs
+++ proyecto-II.hs
@@ -61,8 +61,6 @@ data Persona = Decane | Docente Cargo | NoDocente Area | Estudiante Carrera Ingr
 
 cuantos_doc :: [Persona] -> Cargo -> Int
 cuantos_doc [] c = 0
--- k :: Cargo 
--- c :: Cargo
 cuantos_doc ((Docente k): xs) c | c == k = 1 + cuantos_doc xs c 
                                 | otherwise = cuantos_doc xs c 
 cuantos_doc (_: xs) c = cuantos_doc xs c 
@@ -90,7 +88,8 @@ cuantos_doc' xs c = length (filter (esDocente c) xs)
 
 -- 5A
 
-data Alteracion = Bemol | Sostenido | Natural
+-- data NotaBasica = Do | Re | Mi | Fa | Sol | La | Si deriving (Eq, Ord, Show, Bounded)
+data Alteracion = Bemol | Sostenido | Natural deriving Eq
 data NotaMusical = Nota NotaBasica Alteracion
 
 sonido :: NotaBasica -> Int
@@ -106,19 +105,82 @@ sonido Si = 12
 -- devuelve el sonido de una nota, incrementando en uno su valor si tiene la alteraci ́on Sostenido,
 -- decrementando en uno si tiene la alteraci ́on Bemol y dejando su valor intacto si la alteraci ́on es Natural
 
--- sonidoCromatico :: NotaMusical -> Int
--- sonidoCromatico Nota n a 
--- sonidoCromatico 
+sonidoCromatico :: NotaMusical -> Int
+sonidoCromatico (Nota nb alt) | nb == Si && alt == Sostenido = (sonido Do)
+                              | nb == Do && alt == Bemol = (sonido Si)
+                              | alt ==  Bemol = (sonido nb) - 1
+                              | alt == Sostenido = (sonido nb) + 1
+                              | otherwise = (sonido nb)
+
+
+instance Eq NotaMusical
+  where
+     (Nota n1 a1) == (Nota n2 a2) =
+        sonidoCromatico (Nota n1 a1) == sonidoCromatico (Nota n2 a2)
+
+instance Ord NotaMusical
+  where
+     (Nota n1 a1) <= (Nota n2 a2) =
+        sonidoCromatico (Nota n1 a1) <= sonidoCromatico (Nota n2 a2)
+
+
 
 
 -- 6A
 -- data Maybe a = Nothing | Just a
 
--- primerElemento :: [a] -> Maybe a
--- primerElemento [] = Nothing
--- primerElemento xs = Just (xs!0)
+-- dividir :: Int -> Int -> Maybe Int
+-- dividir x 0 = Nothing
+-- dividir x y = Just ( x `div` y )
+
+primerElemento :: [a] -> Maybe a
+primerElemento [] = Nothing
+primerElemento (x:xs) = Just x
 
 -- 7
--- atender :: Cola -> Maybe Cola
--- atender vaciaC = Nothing
--- atender (Encolada x:xs) = Just xs
+
+data Cola = VaciaC | Encolada Persona Cola 
+
+atender :: Cola -> Maybe Cola
+atender VaciaC = Nothing
+atender (Encolada p c) = Just c
+
+encolar :: Persona -> Cola -> Cola
+encolar p VaciaC = Encolada p VaciaC
+encolar p (Encolada p' c) = Encolada p' (encolar p c) 
+
+busca :: Cola -> Cargo -> Maybe Persona
+busca
+busca
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- 8
+-- data ListaAsoc a b = Vacia | Nodo a b ( ListaAsoc a b )
+
+-- la_concat :: ListaAsoc a b -> ListaAsoc a b -> ListaAsoc a b
+-- la_concat Vacia d = d
+-- la_concat (Nodo a b r) d = Nodo a b ( la_concat r b )
+
+
+-- la_busca :: Eq a => ListaAsoc a b -> a -> Maybe b
+-- la_busca Vacia x = Nothing
+-- la_busca ( Nodo a b xs) x | x == a = Just b 
+--                           | x /=a = la_busca xs x 
